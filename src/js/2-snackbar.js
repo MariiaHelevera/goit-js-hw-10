eceae171e8c4b3a1f45c2581500d563efe33b716
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-02-14 17:39:53 +0100

    tasks done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,72 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+function showNotificationError(delay) {
+    iziToast.error({
+    message: `❌ Rejected promise in ${delay}ms`,
+    position: 'topRight',
+    timeout: 2000,
+    close: false,
+    overlay: false,
+    displayMode: 'once',
+    color: '#EF4040',
+    messageColor: '#FFFFFF',
+    messageSize: '16px',
+    iconColor: '#FFFFFF',
+    icon: '',
+    theme: 'dark',
+    messageLineHeight: '24px',
+    iconColor: '#FFFFFF',
+    });
+}
+
+function showNotificationSuccess(delay) {
+    iziToast.success({
+    message: `✅ Fulfilled promise in ${delay}ms`,
+    position: 'topRight',
+    timeout: 2000,
+    close: false,
+    overlay: false,
+    displayMode: 'once',
+    color: '#59A10D',
+    messageColor: '#FFFFFF',
+    messageSize: '16px',
+    iconColor: '#FFFFFF',
+    icon: '',
+    theme: 'dark',
+    messageLineHeight: '24px',
+    iconColor: '#FFFFFF',
+    });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+    const delay = Number(e.currentTarget.elements.delay.value);
+    const state = e.currentTarget.elements.state.value;
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            }
+            reject(delay);
+        }, delay);
+    });
+
+    promise
+        .then(value => {
+            const message = `✅ Fulfilled promise in ${value}ms`;
+            console.log(message);
+            showNotificationSuccess(value);
+        })
+        .catch(error => {
+            const message = `❌ Rejected promise in ${error}ms`;
+            console.log(message);
+            showNotificationError(error);
+    });
+    
+    e.currentTarget.reset();
+}
